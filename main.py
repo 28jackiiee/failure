1c501771d9b03cc44a33cc0d10d34ef9ca373e3d
28jackiiee <66331105+28jackiiee@users.noreply.github.com>
2023-05-06 13:25:24 -0700

    comitted ez

--- main.py
+++
@@ -1,5 +0,0 @@
-#testing one#
-#hihihihihihihihibhihihih#
-#lol thx for the money and subs u losers hahaha (this is obviously a joke mind you)#
-print("Hello lmao")
-print("I'm here to steal u jkjk this is not happening")
