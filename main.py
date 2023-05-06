6111b8f841afe1622c68daddc5221746a35972e7
Fuzzywiskers <81891298+Fuzzywiskers@users.noreply.github.com>
2023-05-06 15:34:26 -0800

    converting python image to html image

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from getpass import getpass
+from bs4 import BeautifulSoup
 import os
 import replicate 
 
@@ -7,7 +8,12 @@ os.environ["r8_DqBcjBRRLps5L3bsu7b2oNtfBZHsD7e3JJMEV"] = REPLICATE_API_TOKEN
 
 output = replicate.run(
   "stability-ai/stable-diffusion:27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478",
-  input={"prompt": "people having fuck"}
+  input={"prompt": "people having fun"}
 )
 
 print(output)
+
+#moving output image url into html#
+soup = BeautifulSoup("<div id='pyImg'></div>", "html5")
+new_tag = soup.new_tag('img', src=output)
+soup.div.append(new_tag)
