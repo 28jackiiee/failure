da0e23b3af2e6cd25f4c6f1b222a54d2bfb561c6
Fuzzywiskers <81891298+Fuzzywiskers@users.noreply.github.com>
2023-05-06 17:21:15 -0800

    allow tag buttons to work:
    end
    audhf

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ document.querySelector('form.interests').addEventListener('submit', function (e)
 
     console.log(interestsValue.value);    
 });
+
+
+/*making tag buttons*/
+const tagColors = ["#D6EFFF", "#FED18C", "#FEFEFF", "#47E5BC"]
+const tags = document.getElementsByClassName("tag");
+for(let i=0; i<tags.length; i++){
+    tags[i].addEventListener("click", function(){
+        console.log(tags[i]);
+
+        tags[i].style.backgroundColor  = tagColors[3];
+    })
+
+    tags[i].addEventListener("dblclick", function(){
+        tags[i].style.backgroundColor = tagColors[Math.floor(Math.random() * 3)];
+    })
+}
